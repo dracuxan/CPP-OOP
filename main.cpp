82e0ae88b109366beae92f56a9907917b894b93f
dracuxan <khodkenisarg@gmail.com>
2025-04-15 19:11:58 +0530

    add gn build system

--- main.cpp
+++ main.cpp
@@ -7,14 +7,28 @@ public:
   string name;
   string dept;
 
-  void change_dept(string newDept) { dept = newDept; }
+  void change_dept(string newDept) {
+    dept = newDept;
+    salary = 1000;
+    cout << salary << endl;
+  }
+
+private:
+  int salary;
 };
 
 int main() {
   Teacher t1;
 
   t1.name = "Nisarg";
+  t1.dept = "Computers";
   std::cout << t1.name << endl;
+  std::cout << t1.dept << endl;
+
+  t1.change_dept("Arts");
+  std::cout << "New dept: ";
+
+  std::cout << t1.dept << endl;
 
   return 0;
 }
